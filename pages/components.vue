bec75d5d7f5fdc69b066d5497ddc55c5213c9690
Théotime Colin <contact@theotimecolin.com>
2020-10-20 13:54:28 +0200

    feat(Components): start layout

---
+++ pages/components.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="Wrapper">
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-8">
+                    <h1 class="Title Title--l">Buttons</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
